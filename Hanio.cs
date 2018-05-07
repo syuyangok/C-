23f55378db80bafaba740130bdca869b6d2ab935
SUN <syuyangok@gmail.com>
2018-05-07 15:15:22 -0500

    Solution to Hanio with C#

---
+++ Hanio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator x = new Calculator();
+            x.Hanoi(3, 'A', 'B', 'C');
+        }
+
+        class Calculator
+        {
+
+            public void Hanoi(int n, char x, char y, char z)
+            {
+                if (n==1)
+                {
+                    Console.WriteLine("Move from " + x + " to " + z);
+                }
+                else
+                {
+                    Hanoi(n - 1, x, z, y);
+                    Hanoi(1, x, y, z);
+                    Hanoi(n - 1, y, x, z);
+                }
+            }
+
+        }
+    }
+}
