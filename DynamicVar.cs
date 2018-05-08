d0ef3ba7c827e9d13719ce623b311e838bfcc560
SUN <syuyangok@gmail.com>
2018-05-08 10:29:15 -0500

    Sample show dynamic var in C sharp

---
+++ DynamicVar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic myVar = 100;
+            Console.WriteLine(myVar);
+
+            myVar = "Change to string!";
+            Console.WriteLine(myVar);
+        }
+
+        
+    }
+}
